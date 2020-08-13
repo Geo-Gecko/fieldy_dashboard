70463907224269e1995bd35befa7a69a80b1fd13
RonKbS <stephen.bamwite@gmail.com>
2020-08-13 11:43:17 +0300

    add authentication to mapview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import MapView from './components/MapView';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <MapView/>
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute exact path="/">
+            <MapView />
+          </PrivateRoute>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Route } from 'react-router-dom';
+
+const PrivateRoute = ({ children, ...props }) => {
+
+    const urlParams = new URLSearchParams(window.location.search)
+    const x_token = urlParams.get("x-token")
+    if (x_token !== null) {
+        localStorage.setItem("x-token", x_token);
+    }
+    // there is going to be a vulnerability here because we don't confirm if it's the actual token
+    // but we can confirm that once we check for it in the backend.
+    if (!localStorage.getItem('x-token')) {
+        return <Route exact path="/" render={() => {
+            window.location = process.env.REACT_APP_AUTH_SERVICE_URL || "http://localhost:3000"
+            return null
+        }} />
+    }
+
+    window.history.pushState({}, document.title, "/")
+    return <Route {...props}>{children}</Route>;
+};
+
+export default PrivateRoute;
