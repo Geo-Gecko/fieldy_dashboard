4da05fdc0846f7041dc9bedd2ba56bd1199d56bb
RonKbS <stephen.bamwite@gmail.com>
2020-09-09 17:07:38 +0300

    add feature to edit and delete layers

--- src/actions/layerActions.js
+++ src/actions/layerActions.js
@@ -1,7 +1,7 @@
 // import { toast } from 'react-toastify';
 import axiosInstance from './axiosInstance';
 
-export const postPointLayer = postData => dispatch => {
+export const postPointLayer = postData => {
     return axiosInstance
       .post('/layers/listcreatepointlayer/', postData)
       .then(response => {
@@ -12,7 +12,7 @@ export const postPointLayer = postData => dispatch => {
       });
   };
 
-export const postPolygonLayer = postData => dispatch => {
+export const postPolygonLayer = postData => {
 return axiosInstance
     .post('/layers/listcreatepolygonlayer/', postData)
     .then(response => {
@@ -28,6 +28,30 @@ export const getPolygonLayers = () => {
       .get('/layers/listcreatepolygonlayer/')
       .then(response => {
           return response.data
+      })
+      .catch(error => {
+      console.log(error)
+      });
+  };
+
+
+export const updatePolygonLayer = layer_ => {
+  return axiosInstance
+      .put(`/layers/${layer_.properties.field_id}/`, layer_)
+      .then(response => {
+          console.log("Layer has been edited", response.data)
+      })
+      .catch(error => {
+      console.log(error)
+      });
+  };
+
+
+export const deletePolygonLayer = (field_id, layer_) => {
+  return axiosInstance
+      .delete(`/layers/${field_id}/`)
+      .then(() => {
+          console.log("Layer has been deleted")
       })
       .catch(error => {
       console.log(error)
