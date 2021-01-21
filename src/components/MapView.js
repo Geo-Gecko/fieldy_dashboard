a849036eee66c81c8930295eac9197547de80809
dizux <rukuddez@gmail.com>
2021-01-21 11:15:20 +0300

    Requested Changes to Grid for Immediate Push

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -11,8 +11,12 @@ import { ToastContainer } from 'react-toastify';
 
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
-import './popupMod'
-import './popupMod.css'
+import './popupMod';
+import './popupMod.css';
+
+import * as d3ScaleChromatic from 'd3-scale-chromatic';
+import * as d3Scale from 'd3-scale';
+import * as d3Array from 'd3-array';
 
 
 // our components
@@ -34,25 +38,26 @@ L.Icon.Default.mergeOptions({
 });
 
 const { BaseLayer } = LayersControl
-//put the grid here to make it accesbile by the buttons to be added or removed
+//put the grid and maxCount here to make it accesbile by the buttons to be added or removed
 let grid;
+let maxCount = 0;
 
 function inside(point, vs) {
   // ray-casting algorithm based on
   // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
-  
+
   var x = point[0], y = point[1];
-  
+
   var inside = false;
   for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-      var xi = vs[i][0], yi = vs[i][1];
-      var xj = vs[j][0], yj = vs[j][1];
-      
-      var intersect = ((yi > y) != (yj > y))
-          && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-      if (intersect) inside = !inside;
+    var xi = vs[i][0], yi = vs[i][1];
+    var xj = vs[j][0], yj = vs[j][1];
+
+    var intersect = ((yi > y) != (yj > y))
+      && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+    if (intersect) inside = !inside;
   }
-  
+
   return inside;
 };
 
@@ -191,8 +196,8 @@ class MapView extends Component {
       let width = bounds._northEast.lng - bounds._southWest.lng;
       let height = bounds._northEast.lat - bounds._southWest.lat;
       //here we modify the number of gridcells, can be changed to account for closer clusters of gridcells
-      var countX = 5; //cells by x
-      var countY = 5; //cells by y
+      var countX = 10; //cells by x
+      var countY = 10; //cells by y
       var cellWidth = width / countX;
       var cellHeight = height / countY;
 
@@ -220,6 +225,9 @@ class MapView extends Component {
             geometry: {
               type: 'Polygon',
               coordinates: [cell]
+            },
+            properties: {
+              count: 0,
             }
           });
           // refresh cusror for cell
@@ -239,31 +247,52 @@ class MapView extends Component {
 
       //loop through the grid cells here to attach information summarised elsewhere
       grid.eachLayer(layer => {
-        let poly = [[layer.getLatLngs()[0][0].lat,layer.getLatLngs()[0][0].lng],[layer.getLatLngs()[0][1].lat,layer.getLatLngs()[0][1].lng],[layer.getLatLngs()[0][2].lat,layer.getLatLngs()[0][2].lng],[layer.getLatLngs()[0][3].lat,layer.getLatLngs()[0][3].lng]]
+        let poly = [[layer.getLatLngs()[0][0].lat, layer.getLatLngs()[0][0].lng], [layer.getLatLngs()[0][1].lat, layer.getLatLngs()[0][1].lng], [layer.getLatLngs()[0][2].lat, layer.getLatLngs()[0][2].lng], [layer.getLatLngs()[0][3].lat, layer.getLatLngs()[0][3].lng]]
         let count = 0;
         //calcualtes the number (COUNT) of centroids that fall within each polygon (I wish to be able to remove from the array as they are found, but i dont want to spent too much time on that.)
         coordinates.forEach(element => {
           if (inside([element.lat, element.lng], poly)) count++;
         });
+        layer.feature.properties.count = count;
         //bind a popup for now just showing the count of the features per grid cell
         layer.bindPopup("Count: " + count);
 
-        //grid style per gridcell depending on factors, for now just visibility of a cell.
-        layer.setStyle({
-            // the fillColor is adapted from a property which can be changed by the user (segment)
-            // fillColor: this.getColor(feature.properties.scores[this.state.segment]),
-            // weight: 0.3,
-            //stroke-width: to have a constant width on the screen need to adapt with scale 
-            opacity: count > 0 ? 1 : 0 ,
-            // color: ,
-            // dashArray: '3',
-            fillOpacity: count > 0 ? 0.2 : 0
-        })
+        maxCount = count > maxCount ? count : maxCount;
+
 
       });
 
       if (this.refs.myMap) {
+
+        const thresholds = d3Array
+          .range(0, 10)
+          .map(p => Math.pow(2, p));
+        const color = d3Scale
+          .scaleLog()
+          .domain(d3Array.extent(thresholds))
+          .interpolate(() => d3ScaleChromatic.interpolateYlGn);
+
+        grid.eachLayer(layer => {
+          //grid style per gridcell depending on factors, for now just visibility of a cell.
+          layer.setStyle({
+            // the fillColor is adapted from a property which can be changed by the user (segment)
+            fillColor: color(layer.feature.properties.count),
+            weight: 0.3,
+            //stroke-width: to have a constant width on the screen need to adapt with scale
+            opacity: layer.feature.properties.count > 0 ? 1 : 0,
+            // color: ,
+            // dashArray: '3',
+            fillOpacity: layer.feature.properties.count > 0 ? 0.4 : 0
+          })
+
+        })
         this.refs.myMap.leafletElement.addLayer(grid);
+        //this removes the grid when the user zooms in past zoom level 11
+        this.refs.myMap.leafletElement.on('moveend', () => {
+          if (this.refs.myMap.leafletElement.getZoom() > 10) {
+            this.refs.myMap.leafletElement.removeLayer(grid)
+          }
+        })
       }
     }
 
