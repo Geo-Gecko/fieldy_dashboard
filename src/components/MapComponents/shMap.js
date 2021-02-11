649888e759298abe6f49b0dbdd0506ba81d25425
Stephen B Bamwite <40039818+RonKbS@users.noreply.github.com>
2021-02-11 08:18:50 +0000

    add cookie policy and geogecko attribution (#45)

--- src/components/MapComponents/shMap.js
+++ src/components/MapComponents/shMap.js
@@ -15,6 +15,7 @@ import '../popupMod.css'
 
 // local components
 import ShSideBar from '../shSideBar';
+import { CookiesPolicy } from '../cookiesPolicy';
 
 
 delete L.Icon.Default.prototype._getIconUrl;
@@ -35,8 +36,8 @@ let ShMap = ({
   } = state;
   let {
     _saveCurrentView, addGridLayers, removeGridLayers, _onFeatureGroupReady,
-    handleRightClick, _onEdited, _onCreated, _onDeleted, props
-  } = mapInstance
+    handleRightClick, _onEdited, _onCreated, _onDeleted, props, myCookiePref
+  } = mapInstance;
 
   return (
     <React.Fragment>
@@ -48,23 +49,61 @@ let ShMap = ({
       center={currentLocation}
       zoom={zoom}
     >
+      <CookiesPolicy mapInstance={mapInstance} state={state} />
+      {!localStorage.getItem("cookieusagedisplayed") ?
+      <Control 
+        position="bottomright"
+        ref={myCookiePref}
+        >
+        <style type="text/css">
+          {`
+              #cookie_wording {
+                box-shadow: 0 1px 5px rgba(0,0,0,0.65);
+                border-radius: 4px;
+                border: none;
+                background-color: white;
+              }
+              #cookie-view {
+                color: #e15b26;
+                border: none;
+                box-shadow: 0;
+                background-color: transparent;
+              }
+              `}
+        </style>
+        <h6 id="cookie_wording">
+          <button className="current-view" onClick={
+            () => {
+              myMap.current.leafletElement.removeControl(myCookiePref.current.leafletElement)
+              localStorage.setItem('cookieusagedisplayed', true)
+
+            }
+          }>
+            <i className="fa fa-times"></i>
+          </button>
+          {' '}By continuing to view this site, you agree to our usage of
+          <button id="cookie-view" onClick={() => mapInstance.setState({...state, showCookiePolicy: true})}
+            > cookies{' '}.
+          </button>
+        </h6>
+      </Control> : null }
       <LayersControl position="bottomright">
         <BaseLayer name="Google Satellite">
           <TileLayer
             url="https://mt0.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
-            attribution="powered by Google. <br/> Please note this imagery isn't necessarily up to date "
+            attribution={`powered by Google. | &copy; ${new Date().getFullYear()} GeoGecko <br/> Please note this imagery isn't necessarily up to date `}
           />
         </BaseLayer>
         <BaseLayer checked name="OpenStreetMap.BlackAndWhite">
           <TileLayer
-            attribution="&amp;copy <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors  <br/> Please note this imagery isn't necessarily up to date "
+            attribution={`&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors | &copy; ${new Date().getFullYear()} GeoGecko  <br/> Please note this imagery isn't necessarily up to date `}
             url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
           />
         </BaseLayer>
         <BaseLayer name="OpenStreetMap.Mapnik">
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors. <br/> Please note this imagery isn't necessarily up to date "
+            attribution={`&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors. | &copy; ${new Date().getFullYear()} GeoGecko <br/> Please note this imagery isn't necessarily up to date `}
           />
         </BaseLayer>
       </LayersControl>
@@ -121,11 +160,6 @@ let ShMap = ({
       <Control position="topright" >
         <style type="text/css">
           {`
-                .current-view {
-                  box-shadow: 0 1px 5px rgba(0,0,0,0.65);
-                  border-radius: 4px;
-                  border: none;
-                }
                 #field_download_link, field_download_link:hover {
                   color: black;
                   text-decoration: none;
