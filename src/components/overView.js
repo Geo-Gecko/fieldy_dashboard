d00815360137528d65500c1ff59c2169618364c0
RonKbS <stephen.bamwite@gmail.com>
2021-11-18 18:04:12 +0300

    add overview table

--- src/components/overView.js
+++ src/components/overView.js
@@ -1,165 +1,50 @@
 import React from 'react';
 import { Bar, Doughnut } from 'react-chartjs-2';
 
+import BootstrapTable from 'react-bootstrap-table-next';
+
+
 let colours;
-export const OverViewDonutGraph = graphData => {
-
-    let labels = [...new Set(graphData.graphData.map(data_ => data_.cropType))];
-    let count = [...new Set(graphData.graphData.map(data_ => data_.count))];
-    if (!colours) {
-        colours = [...new Set(graphData.graphData.map(data_ => data_.colours))];
-    }
-
-    return (
-        <Doughnut
-            data={
-                {
-                    "labels": labels,
-                    "datasets": [
-                        {
-                            "label": "Number of Fields",
-                            "data": count,
-                            backgroundColor: [
-                              '#63432b',
-                              '#784722',
-                              '#D95e00',
-                              '#7d3d0b',
-                              '#5e391d',
-                              '#db701f',
-                              '#96521e',
-                              '#bf7943',
-                              '#f79143',
-                              '#b05713'
-                            ],
-                            borderColor: [
-                              '#63432b',
-                              '#784722',
-                              '#D95e00',
-                              '#7d3d0b',
-                              '#5e391d',
-                              '#db701f',
-                              '#96521e',
-                              '#bf7943',
-                              '#f79143',
-                              '#b05713'
-                            ],
-                        }
-                    ]
-                }
-            }
-            options={{
-                legend: {
-                    display: true,
-                    position: "bottom",
-                },
-                title: {
-                    display: true,
-                    text: 'Crop Type Representation'
-                },
-                tooltips: {
-                    callbacks: {
-                      label: function(tooltipItem, data) {
-                        var dataset = data.datasets[tooltipItem.datasetIndex];
-                        var meta = dataset._meta[Object.keys(dataset._meta)[0]];
-                        var total = meta.total;
-                        var currentValue = dataset.data[tooltipItem.index];
-                        var percentage = parseFloat((currentValue/total*100).toFixed(1));
-                        return currentValue + ' fields - representing (' + percentage + '%)';
-                      },
-                      title: function(tooltipItem, data) {
-                        return data.labels[tooltipItem[0].index];
-                      }
-                    }
-                  },
-            }}
-            height={140}    
-        />
-    )
-
+let commafy = (value) => {
+  value += '';
+  let x = value.split('.');
+  let x1 = x[0];
+  let x2 = x.length > 1 ? '.' + x[1] : '';
+  let rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+      x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+  return `${x1 + x2}`;
 }
 
+export const OverViewTable = ({ graphData }) => {
 
-export const OverViewBarGraph = graphData => {
-
-    let labels = [...new Set(graphData.graphData.map(it => it.cropType))];
-    let area = [...new Set(graphData.graphData.map(it => (it.area/1000000).toFixed(2)))];
-    if (!colours) {
-        colours = [...new Set(graphData.graphData.map(data_ => data_.colours))];
+  graphData.sort((row1, row2) => {
+    if (parseFloat(row1.area) > parseFloat(row2.area)) {
+      return -1
+    } else if (parseFloat(row1.area) < parseFloat(row2.area)) {
+      return 1
     }
+    return 0
+  })
 
-    return (
-        <Bar
-            data={
-                {
-                    "labels": labels,
-                    "datasets": [
-                        {
-                            "label": "Total Areas",
-                            "data": area,
-                            backgroundColor: [
-                              '#63432b',
-                              '#784722',
-                              '#D95e00',
-                              '#7d3d0b',
-                              '#5e391d',
-                              '#db701f',
-                              '#96521e',
-                              '#bf7943',
-                              '#f79143',
-                              '#b05713'
-                            ],
-                            borderColor: [
-                              '#63432b',
-                              '#784722',
-                              '#D95e00',
-                              '#7d3d0b',
-                              '#5e391d',
-                              '#db701f',
-                              '#96521e',
-                              '#bf7943',
-                              '#f79143',
-                              '#b05713'
-                            ],
-                            "lineTension": 0.1
-                        }
-                    ]
-                }
-            }
-            options={{
-                legend: {
-                    display: false,
-                    position: "bottom",
-                },
-                title: {
-                    display: true,
-                    text: 'Crop Area Coverage'
-                },
-                scales: {
-                    yAxes: [
-                      {
-                        scaleLabel: {
-                          display: true,
-                          labelString: 'Area (sq km)'
-                        },
-                        ticks: {
-                          callback: function(value) {
-                            value += '';
-                            let x = value.split('.');
-                            let x1 = x[0];
-                            let x2 = x.length > 1 ? '.' + x[1] : '';
-                            let rgx = /(\d+)(\d{3})/;
-                            while (rgx.test(x1)) {
-                                x1 = x1.replace(rgx, '$1' + ',' + '$2');
-                            }
-                            return `${(x1 + x2)}`;
-                          }
-                        }
-                      }
-                    ]
-                  }
-            }}
-            height={140}
-        />
-    )
+  graphData.forEach(row => {
+    console.log(row)
+    if (Object.keys(row).length) {
+      row.area = commafy(row.area.toString())
+      row.count = commafy(row.count.toString())
+    }
+  })
+  delete graphData.colours
+  const columns = [
+    { dataField: 'cropType', text: 'Crop Type' },
+    { dataField: 'area', text: 'Crop Area Coverage (sq m)' },
+    { dataField: 'count', text: 'Total Number of fields'}
+  ];
+  return (
+    <React.Fragment>
+      <BootstrapTable keyField='cropType' data={ graphData } columns={ columns } />
+    </React.Fragment> 
+  );
 
 }
