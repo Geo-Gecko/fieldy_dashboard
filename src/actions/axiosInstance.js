25b58ec33510b278a6cab9b7d4b92c6f82eb2a84
RonKbS <stephen.bamwite@gmail.com>
2022-03-23 15:35:13 +0300

    to update this branch:     - log to copy messo, reset softly stash, merge main, stash pop, commit

--- src/actions/axiosInstance.js
+++ src/actions/axiosInstance.js
@@ -7,7 +7,7 @@ let token = localStorage.getItem('x-token');
 // this accounts for missing storage item on first login
 // const urlParams = new URLSearchParams(window.location.search)
 // let url_token = urlParams.get("x-token")
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MTE2NDMxOGVhZWY5MTAwMGFkY2ZlYWMiLCJ1c2VyVHlwZSI6IlZJRVdFUiIsIm1lbWJlck9mIjoiNjExNjQyMDdlYWVmOTEwMDBhZGNmZWFiIiwiYXBpVXNlciI6ZmFsc2UsInBheW1lbnRMZXZlbHMiOiJTRUNPTkQgTEVWRUwiLCJpYXQiOjE2NDg2Mjg4NTd9.7Gsn91veziaMSx-OMepr1ePNthK1N23IhaXq0YgxRK4"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MTE2NDMxOGVhZWY5MTAwMGFkY2ZlYWMiLCJ1c2VyVHlwZSI6IlZJRVdFUiIsIm1lbWJlck9mIjoiNjExNjQyMDdlYWVmOTEwMDBhZGNmZWFiIiwiYXBpVXNlciI6ZmFsc2UsInBheW1lbnRMZXZlbHMiOiJTRUNPTkQgTEVWRUwiLCJpYXQiOjE2NDgwMzg3MTJ9.Q3-8YIiR69u7FoZ4q40EBb8F5mdYoHNlICyOuyHIYTA"
 const headers = token
   ? {
     'Content-Type': 'application/json',
