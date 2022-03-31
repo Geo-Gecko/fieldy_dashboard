e7aa10e66d895973dcb2e694c1adecdbc62f2d1c
RonKbS <stephen.bamwite@gmail.com>
2022-03-31 19:44:40 +0300

    filter fields according to month visited oaf

--- src/actions/oaActions.js
+++ src/actions/oaActions.js
@@ -1,20 +1,20 @@
 import axiosInstance from './axiosInstance';
 import {
-    GET_LAST_VISITS
+  GET_LAST_VISITS
 } from './types';
 
 
 export const getLastVisits = month_ => dispatch => {
-    return axiosInstance
-        .get(`/layers/last-visit-summary/${month_}`)
-        .then(response => {
-            dispatch({
-              type: GET_LAST_VISITS,
-              payload: response.data
-            })
-            return response.data
-        })
-        .catch(error => {
-        console.log(error)
-        });
-    };
+  return axiosInstance
+    .get(`/layers/last-visit-summary/${month_}`)
+    .then(response => {
+      dispatch({
+        type: GET_LAST_VISITS,
+        payload: response.data
+      })
+      return response.data
+    })
+    .catch(error => {
+      console.log(error)
+    });
+};
